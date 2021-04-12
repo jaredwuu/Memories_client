5c54159315c0f5391c6d3b150ee43d1180a593f4
thestreaky <jiachaowu@hotmail.com>
2021-04-12 03:04:56 -0400

    first commit

---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Post from './Post/Post.js'
+import useStyles from './styles'
+const Posts = () => {
+    const classes =useStyles();
+    return (
+        <div>
+            <h1> POSTS </h1>
+            <Post />
+            <Post />
+
+        </div>
+    )
+}
+
+export default Posts
