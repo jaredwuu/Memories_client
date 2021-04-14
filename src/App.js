eb603ca02838fc1582d0a0b2a5b3d15c6065c63e
thestreaky <jiachaowu@hotmail.com>
2021-04-14 02:56:24 -0400

    up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+
 import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+
+import {getPosts} from './actions/posts';
 import memories from './images/memories.png';
 import Posts from './components/Posts/Posts.js'
 import Form from './components/Form/Form'
@@ -7,6 +12,10 @@ import useStyles from './styles'
 
 const App =()=>{
     const classes =useStyles();
+    const dispatch = useDispatch();
+    useEffect(() => {
+       dispatch(getPosts());
+    }, []);
     return(
         <Container maxidth ="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,16 +1,29 @@
 import React from 'react'
+import {Grid, CircularProgress} from '@material-ui/core';
+
+import {useSelector} from 'react-redux'
+
 import Post from './Post/Post.js'
 import useStyles from './styles'
 const Posts = () => {
+    const posts = useSelector(state => state.posts)
     const classes =useStyles();
-    return (
-        <div>
-            <h1> POSTS </h1>
-            <Post />
-            <Post />
 
-        </div>
-    )
-}
+
+    return (
+        
+    
+         !posts.length ? <CircularProgress />:(
+            <Grid className = {classes.container} container alignItems = "stretch" spacing ={3} >
+                 {posts.map((post)=>(
+                     <Grid key ={post._id} item xs={12} sm={6}>
+                         <Post post={post} /> 
+                    </Grid>
+             ))}
+           </Grid>
+         )
+
+    );
+};
 
 export default Posts
